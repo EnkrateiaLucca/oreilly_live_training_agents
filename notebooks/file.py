144b16333302d8bd0c8db619eb53b1dab451f14b
Lucas Soares <new_email_address@example.com>
2024-01-23 19:07:13 +0000

    Add code to compare pancakes and waffles

---
+++ notebooks/file.py
@@ -0,0 +1,8 @@
+pancakes = ['Delicious', 'Fluffy', 'Versatile']
+
+waffles = ['Crispy', 'Golden', 'Syrup pockets']
+
+for i in range(len(pancakes)):
+    print(f'Pancakes are {pancakes[i]}')
+    print(f'Waffles are {waffles[i]}')
+    print()  # Add a blank line for readability
