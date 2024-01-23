7c646c88115d6fd91dddb23328da130866c0e865
Lucas Soares <new_email_address@example.com>
2024-01-23 16:06:55 +0000

    added a notebook showing how to set up chat with website

---
+++ notebooks/chat-with-pdf/app/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from rag_chroma import chain as rag_chroma_chain
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+add_routes(app, rag_chroma_chain, path="/rag-chroma")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
